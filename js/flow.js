3a4ee2ef6e9ab5710f07995fe1448224b6261733
yamamaki <dingzhm93@gmail.com>
2015-02-16 10:34:27 +0800

    fix silly bug in rectTrace()

--- js/flow.js
+++ js/flow.js
@@ -5,15 +5,16 @@ var per_x = undefined;
 var per_y = undefined;
 var bp = [];
 var ep_pair = [];
+var background = "#065688";
 var dir = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]];
 var default_settings = {
     "width": undefined,
     "height": undefined,
-    "num_of_row": 3,
-    "num_of_col": 4,
-    "num_of_line": 4,
-    "max_vv": 5,
-    "max_vc": 5,
+    "num_of_row": 2,
+    "num_of_col": 2,
+    "num_of_line": 2,
+    "max_vv": 1,
+    "max_vc": 1,
     "max_z": 100,
 }
 
@@ -46,9 +47,12 @@ function Flow(canvas) {
                         window.setTimeout(callback, 1000 / 60);
                     });
             }();
-
-
-        }
+            var ctx = canvas.getContext("2d");
+            (function demo() {
+                draw(ctx, default_settings.width, default_settings.height);
+                requestAnimFrame(demo);
+            }) ();
+        },
         destroy: function () {
             ep_pair = [];
             bp = [];
@@ -90,8 +94,8 @@ function Basispoint(row, col) {
     this.y = rrandom(per_y, col*per_y);
     this.z = rrandom(default_settings.max_z); // the darkness or lightness of the color depends on the value of z-dimension ;
     this.is_ep = false; // whether on the line or not ;
-    this.vv = rrandom(default_settings.max_vv); // vv: velocity vector ;
-    this.vc = rrandom(default_settings.max_vc); // vc: velocity for color changes ;
+    this.vv = default_settings.max_vv*(Math.random()*2-1); // vv: velocity vector ;
+    this.vc = default_settings.max_vc*(Math.random()*2-1); // vc: velocity for color changes ;
 }
 
 /**
@@ -138,8 +142,6 @@ function get_endpoint_pair(row, col) {
             if (next_ep.is_ep == true) {
                 continue;
             }
-            console.log("ep", ep.pos);
-            console.log("next_ep", next_ep.pos);
             next_ep.is_ep = true;
             ep.is_ep = true;
             ep_pair.push([ep.pos, next_ep.pos]);
@@ -157,15 +159,8 @@ function movement(ep_pair) {
         ep = getPointByPos(ep_pair[i][0]);
         ep_ = getPointByPos(ep_pair[i][1]);
         ang = Math.atan((ep.y-ep_.y)/(ep.x-ep_.x));
-        epx = ep.x+ep.vv*Math.cos(ang);
-        epy = ep.y+ep.vv*Math.sin(ang);
-        epz = ep.z+ep.vc;
-        ep_x = ep_.x+ep_.vv*Math.cos(ang);
-        ep_y = ep_.y+ep_.vv*Math.sin(ang);
-        ep_z = ep_.z+ep_.vc;
-        ep = recTrace(ep, epx, epy);
-        ep_ = recTrace(ep_, ep_x, ep_y);
-     
+        recTrace(ep, ang);
+        recTrace(ep_, ang);
     }
 }
 
@@ -173,21 +168,49 @@ function getPointByPos(pos) {
     return bp[pos[0]][pos[1]];
 }
 
-function recTrace(ep, epx, epy) {
-    per_x = default_setttings.width/default_settings.num_of_col;
-    per_y = default_setttings.height/default_setttings.num_of_row;
-    delta_x = epx-ep.x;
-    delta_y = epy-ep.y;
+function recTrace(ep, ang) {
+    per_x = default_settings.width/default_settings.num_of_col;
+    per_y = default_settings.height/default_settings.num_of_row;
     max_x = (ep.pos[1]+1)*per_x;
     min_x = ep.pos[1]*per_x;
     max_y = (ep.pos[0]+1)*per_y; 
-    min_y = ep.pos[0]*per_y
-    if ((ep.x-max_x)*(ep.x-min_x) >= 0 || (ep.y-max_y)*(ep.y-min_y)) {
-        n.vv *= -1;
-        n.vc *= -1;
-        ep.x -= delta_x;
-        ep.y -= delta_y;
+    min_y = ep.pos[0]*per_y;
+    ep.x += ep.vv*Math.cos(ang);
+    ep.y += ep.vv*Math.sin(ang);
+    ep.z += ep.vc;
+    if ((ep.x-max_x)*(ep.x-min_x) >= 0 || (ep.y-max_y)*(ep.y-min_y) >= 0) {
+        ep.x -= 2*ep.vv*Math.cos(ang);
+        ep.y -= 2*ep.vv*Math.sin(ang);
+        ep.z -= 2*ep.vc;
+        ep.vv *= -1;
+        ep.vc *= -1;
     }
-    return ep;
 }
 
+function draw(ctx, w, h) {
+    num_of_row = default_settings.num_of_row;
+    num_of_col = default_settings.num_of_col;
+    ctx.clearRect(0,0,w,h);
+    movement(ep_pair);
+    for (var i=0; i<num_of_row-1; ++i) {
+        for (var j=0; j<num_of_col-1; ++j) {
+            ctx.fillStyle = background;
+            ctx.beginPath();
+            ctx.moveTo(bp[i][j].x, bp[i][j].y);
+            ctx.lineTo(bp[i][j+1].x, bp[i][j+1].y);
+            ctx.lineTo(bp[i+1][j+1].x, bp[i+1][j+1].y);
+            ctx.closePath();
+            ctx.fill()
+
+            ctx.fillStyle = background;
+            ctx.beginPath();
+            ctx.moveTo(bp[i][j].x, bp[i][j].y);
+            ctx.lineTo(bp[i+1][j].x, bp[i+1][j].y);
+            ctx.lineTo(bp[i+1][j+1].x, bp[i+1][j+1].y);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+
+
+}
